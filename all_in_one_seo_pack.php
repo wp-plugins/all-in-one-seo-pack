0e3fb332a6623554163f7cf9714cba50028e50db
uberdose <uberdose@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-08-08 04:13:47 +0000

    fixed the problem with title tags spread on several lines (yes, some themes do this)
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/trunk@16496 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- all_in_one_seo_pack.php
+++ all_in_one_seo_pack.php
@@ -4,7 +4,7 @@
 Plugin Name: All in One SEO Pack
 Plugin URI: http://wp.uberdose.com/2007/03/24/all-in-one-seo-pack/
 Description: Out-of-the-box SEO for your Wordpress blog.
-Version: 1.2.3.7
+Version: 1.2.3.8
 Author: uberdose
 Author URI: http://wp.uberdose.com/
 */
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
 class All_in_One_SEO_Pack {
 	
- 	var $version = "1.2.3.7";
+ 	var $version = "1.2.3.8";
  	
  	/**
  	 * Number of words to be used (max) for generating an excerpt.
@@ -137,7 +137,7 @@ class All_in_One_SEO_Pack {
 	
 	function replace_title($content, $title) {
 		$title = trim(stripslashes(addslashes($title)));
-		$header = preg_replace_callback("/<title>.*<\/title>/",
+		$header = preg_replace_callback("/<title>.*<\/title>/s",
 			create_function('$match_not_needed',"return '<title>$title</title>';"), $content);
 		return $header;
 	}

031f8e7f82ad85cc3bca67891f0ae592ec05532c
uberdose <uberdose@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-28 15:05:42 +0000

    bulgarian translation
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/trunk@20308 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- all_in_one_seo_pack.php
+++ all_in_one_seo_pack.php
@@ -4,7 +4,7 @@
 Plugin Name: All in One SEO Pack
 Plugin URI: http://wp.uberdose.com/2007/03/24/all-in-one-seo-pack/
 Description: Out-of-the-box SEO for your Wordpress blog.
-Version: 1.2.7.8
+Version: 1.2.7.9
 Author: uberdose
 Author URI: http://wp.uberdose.com/
 */
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
 class All_in_One_SEO_Pack {
 	
- 	var $version = "1.2.7.8";
+ 	var $version = "1.2.7.9";
  	
  	/**
  	 * Max numbers of chars in auto-generated description.
@@ -824,7 +824,7 @@ class All_in_One_SEO_Pack {
 <p class="submit">
 <input type="hidden" name="action" value="aiosp_update" /> 
 <input type="hidden" name="page_options" value="aiosp_home_description" /> 
-<input type="submit" name="Submit" value="<?php _e('Update Options')?> &raquo;" /> 
+<input type="submit" name="Submit" value="<?php _e('Update Options', 'all_in_one_seo_pack')?> &raquo;" /> 
 </p>
 </form>
 </div>

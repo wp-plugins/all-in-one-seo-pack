b7b78e788357f148be5f193acf0ae4c870a9aaf2
uberdose <uberdose@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-27 10:54:53 +0000

    1.3.5.7 the_title applied to title
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/trunk@23371 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- all_in_one_seo_pack.php
+++ all_in_one_seo_pack.php
@@ -4,7 +4,7 @@
 Plugin Name: All in One SEO Pack
 Plugin URI: http://wp.uberdose.com/2007/03/24/all-in-one-seo-pack/
 Description: Out-of-the-box SEO for your Wordpress blog.
-Version: 1.3.5.6
+Version: 1.3.5.7
 Author: uberdose
 Author URI: http://wp.uberdose.com/
 */
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
 class All_in_One_SEO_Pack {
 	
- 	var $version = "1.3.5.6";
+ 	var $version = "1.3.5.7";
  	
  	/**
  	 * Max numbers of chars in auto-generated description.
@@ -206,6 +206,7 @@ class All_in_One_SEO_Pack {
 	
 	function replace_title($content, $title) {
 		$title = trim(strip_tags($title));
+		$title = apply_filters('the_title', $title);
 		$title_tag_start = "<title>";
 		$title_tag_end = "</title>";
 		$len_start = strlen($title_tag_start);

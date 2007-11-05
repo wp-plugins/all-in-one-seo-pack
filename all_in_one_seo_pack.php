2460b774cf7c8fc925aa2bf0c1ceaa4e406953a5
uberdose <uberdose@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-11-05 15:38:05 +0000

    1.3.6.6 German translation
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/trunk@24093 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- all_in_one_seo_pack.php
+++ all_in_one_seo_pack.php
@@ -4,7 +4,7 @@
 Plugin Name: All in One SEO Pack
 Plugin URI: http://wp.uberdose.com/2007/03/24/all-in-one-seo-pack/
 Description: Out-of-the-box SEO for your Wordpress blog.
-Version: 1.3.6.5
+Version: 1.3.6.6
 Author: uberdose
 Author URI: http://wp.uberdose.com/
 */
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
 class All_in_One_SEO_Pack {
 	
- 	var $version = "1.3.6.5";
+ 	var $version = "1.3.6.6";
  	
  	/**
  	 * Max numbers of chars in auto-generated description.

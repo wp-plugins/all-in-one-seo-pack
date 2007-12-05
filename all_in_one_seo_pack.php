b1497834baeba2874a5d23d02888247a0a8c7326
uberdose <uberdose@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-12-05 20:34:13 +0000

    1.4.3.1 removed comments from top of the file
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/trunk@26137 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- all_in_one_seo_pack.php
+++ all_in_one_seo_pack.php
@@ -1,14 +1,10 @@
-1.4.3
-paged title format as option
-archives, tags and categories have paged titles now
-
 <?php
 
 /*
 Plugin Name: All in One SEO Pack
 Plugin URI: http://wp.uberdose.com/2007/03/24/all-in-one-seo-pack/
 Description: Out-of-the-box SEO for your Wordpress blog.
-Version: 1.4.3
+Version: 1.4.3.1
 Author: uberdose
 Author URI: http://wp.uberdose.com/
 */
@@ -32,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
 class All_in_One_SEO_Pack {
 	
- 	var $version = "1.4.3";
+ 	var $version = "1.4.3.1";
  	
  	/** Max numbers of chars in auto-generated description */
  	var $maximum_description_length = 160;

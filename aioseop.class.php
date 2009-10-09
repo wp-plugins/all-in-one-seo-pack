dc4fbfda7120d314e1ecaeb9663306a88eb523de
hallsofmontezuma <hallsofmontezuma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-09 18:59:53 +0000

    switched to fetch_feed, no version bump
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/trunk@161947 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- aioseop.class.php
+++ aioseop.class.php
@@ -1543,6 +1543,68 @@ href="http://twitter.com/michaeltorbert/"><img src="<?php //echo WP_PLUGIN_URL; 
 <h3><?php _e('Click on option titles to get help!', 'all_in_one_seo_pack') ?></h3>
 
 <?php
+
+if(function_exists('fetch_feed')){ 
+	// start new feed
+?>
+<?php echo "Highest Donations"; ?>
+<?php // Get RSS Feed(s)
+include_once(ABSPATH . WPINC . '/feed.php');
+
+// Get a SimplePie feed object from the specified feed source.
+$rss = fetch_feed('feed://donations.semperfiwebdesign.com/category/highest-donations/feed/');
+
+// Figure out how many total items there are, but limit it to 5. 
+$maxitems = $rss->get_item_quantity(3); 
+
+// Build an array of all the items, starting with element 0 (first element).
+$rss_items = $rss->get_items(0, $maxitems); 
+?>
+
+<ul>
+    <?php if ($maxitems == 0) echo '<li>No items.</li>';
+    else
+    // Loop through each feed item and display each item as a hyperlink.
+    foreach ( $rss_items as $item ) : ?>
+    <li>
+        <a href='<?php echo $item->get_permalink(); ?>'
+        title='<?php echo 'Posted '.$item->get_date('j F Y | g:i a'); ?>'>
+        <?php echo $item->get_title(); ?></a>
+    </li>
+    <?php endforeach; ?>
+</ul>
+
+<?php echo "Latest Donations"; ?>
+<?php // Get RSS Feed(s)
+include_once(ABSPATH . WPINC . '/feed.php');
+
+// Get a SimplePie feed object from the specified feed source.
+$rss = fetch_feed('feed://donations.semperfiwebdesign.com/category/all-in-one-seo-pack/feed/');
+
+// Figure out how many total items there are, but limit it to 5. 
+$maxitems = $rss->get_item_quantity(3); 
+
+// Build an array of all the items, starting with element 0 (first element).
+$rss_items = $rss->get_items(0, $maxitems); 
+?>
+
+<ul>
+    <?php if ($maxitems == 0) echo '<li>No items.</li>';
+    else
+    // Loop through each feed item and display each item as a hyperlink.
+    foreach ( $rss_items as $item ) : ?>
+    <li>
+        <a href='<?php echo $item->get_permalink(); ?>'
+        title='<?php echo 'Posted '.$item->get_date('j F Y | g:i a'); ?>'>
+        <?php echo $item->get_title(); ?></a>
+    </li>
+    <?php endforeach; ?>
+</ul>
+
+
+<?php // end new feed
+}else{
+
 $uri = "feed://donations.semperfiwebdesign.com/category/highest-donations/feed/";
 include_once(ABSPATH . WPINC . '/rss.php');
 $rss = fetch_rss($uri);
@@ -1591,7 +1653,9 @@ title='<?php echo $item['title']; ?>'>
 </ul>
 <?php } }else{
 	//fall back on something else for feed here
-}?>
+}
+}
+?>
 
 <?php
 global $wpdb;

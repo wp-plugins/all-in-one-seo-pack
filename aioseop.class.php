fed1ae72ecbea9ad18225ec18911d9e7c398dab1
hallsofmontezuma <hallsofmontezuma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-09 07:07:13 +0000

    fixed typos, no version bump
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/trunk@161760 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- aioseop.class.php
+++ aioseop.class.php
@@ -1473,7 +1473,7 @@ href="http://twitter.com/michaeltorbert/"><img src="<?php //echo WP_PLUGIN_URL; 
 
 <div style="width:824px;">
 <div style="float:left;background-color:white;padding: 10px 10px 10px 10px;margin-right:15px;border: 1px solid #ddd;">
-<div style="width:350px;height:115px;">
+<div style="width:350px;height:130px;">
 	<h3>Donate</h3>
 <em>If you like this plugin and find it useful, help keep this plugin free and actively developed by clicking the <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mrtorbert%40gmail%2ecom&item_name=All%20In%20One%20SEO%20Pack&item_number=Support%20Open%20Source&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8" target="_blank"><strong>donate</strong></a> button or send me a gift from my <a href="https://www.amazon.com/wishlist/1NFQ133FNCOOA/ref=wl_web" target="_blank"><strong>Amazon wishlist</strong></a>.  Also, don't forget to follow me on <a href="http://twitter.com/michaeltorbert/" target="_blank"><strong>Twitter</strong></a>.</em>
 </div>
@@ -1491,8 +1491,8 @@ href="http://twitter.com/michaeltorbert/"><img src="<?php //echo WP_PLUGIN_URL; 
 </div>
 
 <div style="float:left;background-color:white;padding: 10px 10px 10px 10px;border: 1px solid #ddd;">
-<div style="width:415px;height:115px;">
-	<h3>Pageline Themes</h3>
+<div style="width:415px;height:130px;">
+	<h3>PageLines Themes</h3>
 	We would also like to recommend <a href="http://www.pagelines.com/wpthemes/">PageLines</a> for Professional WordPress Themes.  They are attractive, affordable, performance optimized themes that integrate perfectly with All in One SEO Pack to put your professional website at the top of the rankings.
 </div>
 	<a target="_blank" title="iBlogPro" href="http://www.pagelines.com/wpthemes/">

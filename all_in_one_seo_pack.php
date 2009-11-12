43910ff479cb61e50e51e7f1c89c52d14c44092d
hallsofmontezuma <hallsofmontezuma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-12 22:06:29 +0000

    taggins 1.6.8.1
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/tags/1.6.8.1@172764 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- all_in_one_seo_pack.php
+++ all_in_one_seo_pack.php
@@ -4,7 +4,7 @@
 Plugin Name: All in One SEO Pack
 Plugin URI: http://semperfiwebdesign.com
 Description: Out-of-the-box SEO for your Wordpress blog. <a href="options-general.php?page=all-in-one-seo-pack/aioseop.class.php">Options configuration panel</a> | <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mrtorbert%40gmail%2ecom&item_name=All%20In%20One%20SEO%20Pack&item_number=Support%20Open%20Source&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8">Donate</a> | <a href="http://semperfiwebdesign.com/forum/" >Support</a> |  <a href="https://www.amazon.com/wishlist/1NFQ133FNCOOA/ref=wl_web" target="_blank" title="Amazon Wish List">Amazon Wishlist</a>
-Version: 1.6.7
+Version: 1.6.8.1
 Author: Michael Torbert
 Author URI: http://michaeltorbert.com
 */
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*******************************************************************************************************/
-register_activation_hook(__FILE__,'aioseop_activate_pl');
+//register_activation_hook(__FILE__,'aioseop_activate_pl');
 
 $UTF8_TABLES['strtolower'] = array(
 	"Ｚ" => "ｚ",	"Ｙ" => "ｙ",	"Ｘ" => "ｘ",
@@ -554,7 +554,7 @@ echo "<div class='updated fade' style='background-color:green;border-color:green
 }
 
 function aioseop_get_version(){
-	return '1.6.7';
+	return '1.6.8.1';
 }
 
 
@@ -615,7 +615,7 @@ echo "<div class='updated fade' style='background-color:green;border-color:green
 function aioseop_activation_notice(){
 	global $aioseop_options;
 				if(function_exists('admin_url')){
-				echo '<div class="error fade" style="background-color:red;"><p><strong>All in One SEO Pack has new options. Go to <a href="' . admin_url( 'options-general.php?page=all-in-one-seo-pack/aioseop.class.php' ) . '">the admin page</a> to enable and configure the plugin.</strong></p></div>';
+				echo '<div class="error fade" style="background-color:red;"><p><strong>All in One SEO Pack must be configured. Go to <a href="' . admin_url( 'options-general.php?page=all-in-one-seo-pack/aioseop.class.php' ) . '">the admin page</a> to enable and configure the plugin.</strong></p></div>';
 }else{
 		echo '<div class="error fade" style="background-color:red;"><p><strong>All in One SEO Pack must be configured. Go to <a href="' . get_option('siteurl') . 'options-general.php?page=all-in-one-seo-pack/aioseop.class.php' . '">the admin page</a> to enable and configure the plugin.</strong></p></div>';
 }

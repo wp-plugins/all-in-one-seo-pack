cd3cd23ec6a589269ae84ee836b2b12502172122
hallsofmontezuma <hallsofmontezuma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-18 22:21:08 +0000

    update ml
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-seo-pack/trunk@301412 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- aioseop.class.php
+++ aioseop.class.php
@@ -1446,9 +1446,12 @@ if (function_exists('load_plugin_textdomain')) {
 ?>
 	<div id="dropmessage" class="updated" style="display:none;"></div>
 	<div class="wrap">
+		
 		<h2><?php _e('All in One SEO Plugin Options', 'all_in_one_seo_pack'); ?></h2>
 		by <strong>Michael Torbert</strong> of <strong>Semper Fi Web Design</strong>
 		<p>
+			<div style="float:left;">
+			
 		<?php //_e("This is version ", 'all_in_one_seo_pack') ?><?php //_e("$this->version ", 'all_in_one_seo_pack') ?>
 &nbsp;<a target="_blank" title="<?php _e('All in One SEO Plugin Release History', 'all_in_one_seo_pack')?>"
 href="http://semperfiwebdesign.com/documentation/all-in-one-seo-pack/all-in-one-seo-pack-release-history/"><?php _e("Changelog", 'all_in_one_seo_pack')?>
@@ -1461,6 +1464,22 @@ href="http://semperfiwebdesign.com/portfolio/wordpress/wordpress-plugins/forum/"
 href="http://semperfiwebdesign.com/documentation/all-in-one-seo-pack/translations-for-all-in-one-seo-pack/"><?php _e('Translations', 'all_in_one_seo_pack') ?></a>
 | <strong><a target="_blank" title="<?php _e('Pro Version', 'all_in_one_seo_pack') ?>"
 href="http://wpplugins.com/plugin/50/all-in-one-seo-pack-pro-version"><?php _e('UPGRADE TO PRO VERSION', 'all_in_one_seo_pack') ?></a></strong>
+</div>
+
+<div style="float:left;width:370px;margin-left:10px;">
+	<form action="http://semperfiwebdesign.us1.list-manage.com/subscribe/post?u=794674d3d54fdd912f961ef14&amp;id=af0a96d3d9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+		<span>Join our mailing list for tips, tricks, and WordPress secrets.<em><strong>Sign up today and receive a free copy of the e-book 5 SEO Tips for WordPress</strong></em>.</span>
+		<div>
+	<label for="mce-EMAIL">Email Address </label>
+	<input type="text" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+
+		
+			<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn">
+			</div>
+	</form>
+	</div>
+	<div stlye="clear:both;">
+
 <br />
 
 <!--<div style="width:75%;background-color:yellow;">
@@ -1509,6 +1528,22 @@ href="http://twitter.com/michaeltorbert/"><img src="<?php //echo WP_PLUGIN_URL; 
 			<a title="Backup WordPress Easily" href="http://pluginbuddy.com/wordpress-backup/" target="_blank"><img src="http://ithemes.com/graphics/ads/bb-AIO-ad.png" alt="Backup WordPress Easily"></a> 
     </div>
 
+<!--
+	<div style="float:left;background-color:white;padding: 10px 10px 10px 10px;border: 1px solid #ddd;">
+		<div style="width:365px;height:130px;">
+			
+			<form action="http://semperfiwebdesign.us1.list-manage.com/subscribe/post?u=794674d3d54fdd912f961ef14&amp;id=af0a96d3d9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+				<span>Join our mailing list for tips, tricks, and WordPress secrets. Also receive discounts on top commercial plugins and themes.<br /><em><strong>Sign up today and receive a free copy of the e-book 5 SEO Tips for WordPress</strong></em>.</span>
+			<div class="mc-field-group">
+			<label for="mce-EMAIL">Email Address </label>
+			<input type="text" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+				
+					<div><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn"></div>
+			</form>
+</div>
+</div>
+-->
 </div>
 <div style="clear:both";></div>
 <!--
